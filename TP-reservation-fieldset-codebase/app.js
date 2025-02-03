7acabe65af942c662c977149e2192b70f6e5d022
Guillaume Soulichanh <g.soulichanh@gmail.com>
2025-02-03 01:03:16 +0100

    specifications fonctionnelles

---
+++ TP-reservation-fieldset-codebase/app.js
@@ -0,0 +1,20 @@
+/*
+Specification fonctionnelle:
+- L'utilisateur peut ajouter plusieurs réservations.
+- Chaque réservation doit contenir :
+ . Une date sélectionnable (parmi les options définies).
+ . Un nombre de places (minimum 1).
+- l'utilisateur ne peut pas ajouter une nouvelle réservation tant que la précédente n’a pas de date sélectionnée.
+- l'utilisateur ne peut pas choisir plusieurs fois la même date.
+- Permettre de supprimer une réservation.
+- Vérifier les entrées avant soumission.
+*/ 
+
+let datesToDisplay = [
+    { value: "2024-01-06" },
+    { value: "2024-01-13" },
+    { value: "2024-01-20" },
+    { value: "2024-01-27" },
+];
+
+
